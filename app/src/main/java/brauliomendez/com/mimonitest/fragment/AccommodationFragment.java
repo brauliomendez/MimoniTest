37187450803a170e509f250c54c9294229922838
BraulioMendez <braauliomendez@ciencias.unam.mx>
2017-01-13 02:23:23 -0600

    NEW: Added Accommodation Adapter

--- app/src/main/java/brauliomendez/com/mimonitest/fragment/AccommodationFragment.java
+++ app/src/main/java/brauliomendez/com/mimonitest/fragment/AccommodationFragment.java
@@ -3,12 +3,18 @@ package brauliomendez.com.mimonitest.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import brauliomendez.com.mimonitest.R;
+import brauliomendez.com.mimonitest.adapter.AccommodationAdapter;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import mx.leo.easyrecycler.util.RecyclerViewItemClickListener;
+import mx.leo.easyrecycler.util.extensions.RecyclerViewExtensionsKt;
 
 /**
  * Created by Braulio on 13/01/2017.
@@ -16,10 +22,28 @@ import butterknife.ButterKnife;
 
 public class AccommodationFragment extends Fragment {
 
+    @BindView(R.id.accommodation_recycler_view) RecyclerView recyclerView;
+
     @Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                                                  @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_accommodation, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        AccommodationAdapter accommodationAdapter = new AccommodationAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(accommodationAdapter);
+        RecyclerViewExtensionsKt.OnItemClickListener(recyclerView, new RecyclerViewItemClickListener.OnItemClickListener() {
+            @Override public void onItemClick(View view, Integer integer) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/brauliomendez/com/mimonitest/viewholder/AccommodationViewHolder.java
@@ -0,0 +1,35 @@
+package brauliomendez.com.mimonitest.viewholder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import brauliomendez.com.mimonitest.R;
+import brauliomendez.com.mimonitest.model.Accommodation;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import mx.leo.easyrecycler.viewholder.EasyItemViewHolder;
+
+/**
+ * Created by Braulio on 13/01/2017.
+ */
+
+public class AccommodationViewHolder extends EasyItemViewHolder {
+
+    @BindView(R.id.accommodation_image_view) ImageView accommodationImageView;
+    @BindView(R.id.price_text_view) TextView priceTextView;
+    @BindView(R.id.name_text_view) TextView nameTextView;
+
+    public AccommodationViewHolder(View view) {
+        super(view);
+        ButterKnife.bind(this, view);
+    }
+
+    public void bindItem(Accommodation accommodation){
+        Glide.with(accommodationImageView.getContext()).load(accommodation.getImage()).into(accommodationImageView);
+        priceTextView.setText(accommodation.getPrice());
+        nameTextView.setText(accommodation.getName());
+    }
+}
