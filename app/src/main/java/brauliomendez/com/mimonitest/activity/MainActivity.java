02dc34d8f6a91b6fb380a52a1dd0222950bcc729
BraulioMendez <braauliomendez@ciencias.unam.mx>
2017-01-13 01:10:14 -0600

    NEW: Added Color In Tab Layout.

--- app/src/main/java/brauliomendez/com/mimonitest/activity/MainActivity.java
+++ app/src/main/java/brauliomendez/com/mimonitest/activity/MainActivity.java
@@ -1,14 +1,42 @@
 package brauliomendez.com.mimonitest.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import brauliomendez.com.mimonitest.R;
+import brauliomendez.com.mimonitest.adapter.ViewPagerAdapter;
+import brauliomendez.com.mimonitest.fragment.AccommodationFragment;
+import brauliomendez.com.mimonitest.fragment.ExperiencesFragment;
+import brauliomendez.com.mimonitest.fragment.PersonalFragment;
+import brauliomendez.com.mimonitest.fragment.PlacesFragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_view_pager) ViewPager viewPager;
+    @BindView(R.id.main_tab_layout) TabLayout tabLayout;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpViewPager();
+        setUpTabLayout();
+    }
+
+    private void setUpViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PersonalFragment(), getResources().getString(R.string.name_personal_fragment));
+        adapter.addFragment(new AccommodationFragment(), getResources().getString(R.string.name_accommodation_fragment));
+        adapter.addFragment(new ExperiencesFragment(), getResources().getString(R.string.name_experiencies_fragment));
+        adapter.addFragment(new PlacesFragment(), getResources().getString(R.string.name_places_fragment));
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setUpTabLayout(){
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
