f5f683b11c323b759a590bde537b5a22312c216e
BraulioMendez <braauliomendez@ciencias.unam.mx>
2017-01-12 23:52:06 -0600

    NEW: Added Basic Packages.

--- app/src/main/java/brauliomendez/com/mimonitest/activity/MainActivity.java
+++ app/src/main/java/brauliomendez/com/mimonitest/activity/MainActivity.java
@@ -1,12 +1,13 @@
-package brauliomendez.com.mimonitest;
+package brauliomendez.com.mimonitest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import brauliomendez.com.mimonitest.R;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
