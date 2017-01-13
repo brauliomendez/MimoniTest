e34ab140703d439ce64c8664efc36414f0340884
BraulioMendez <braauliomendez@ciencias.unam.mx>
2017-01-13 00:21:18 -0600

    NEW: Added Main Fragments.

---
+++ app/src/main/java/brauliomendez/com/mimonitest/fragment/ExperiencesFragment.java
@@ -0,0 +1,25 @@
+package brauliomendez.com.mimonitest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import brauliomendez.com.mimonitest.R;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Braulio on 13/01/2017.
+ */
+
+public class ExperiencesFragment extends Fragment {
+
+    @Nullable @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                                 @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_experiencies, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
