17a08b8b94be438a1aad728a64a0fa66db400ff4
BraulioMendez <braauliomendez@ciencias.unam.mx>
2017-01-15 23:59:38 -0600

    NEW: Added Message Fragment.

--- app/src/main/java/brauliomendez/com/mimonitest/fragment/MessageFragment.java
+++ app/src/main/java/brauliomendez/com/mimonitest/fragment/MessageFragment.java
@@ -3,12 +3,14 @@ package brauliomendez.com.mimonitest.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import brauliomendez.com.mimonitest.R;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Braulio on 15/01/2017.
@@ -21,5 +23,13 @@ public class MessageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick(R.id.explore_button) void goToMainActivity(){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.main_frame_layout, new ServicesFragment())
+                .commit();
     }
 }
